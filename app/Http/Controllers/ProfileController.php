9d0c05cf3a8d5009b7907d7a2533c49c9f816624
roma <roma@unictive.com>
2022-08-11 10:01:29 +0700

    update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ProfileController extends Controller
 {
-    
+    public function index()
+    {
+        $user = User::findOrFail(Auth::user()->id);
+    }
 }
