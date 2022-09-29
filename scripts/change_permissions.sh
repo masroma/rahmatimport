f6ca5c69d8f25bf262957bf2ba7cf3fea4f0a8e1
reiparmad <106718994+reiparmad@users.noreply.github.com>
2022-09-29 19:56:21 +0700

    edit script change_permission.sh

--- scripts/change_permissions.sh
+++ scripts/change_permissions.sh
@@ -2,7 +2,7 @@
 
 # Fix user rights
 sudo usermod -a -G apache ec2-user
-sudo chown -R ec2-user:apache /var/www
-sudo chmod 2775 /var/www
-find /var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0664 {} \;
+sudo chown -R ec2-user:apache /var/www/html
+sudo chmod 2775 /var/www/html
+find /var/www/html -type d -exec sudo chmod 2775 {} \;
+find /var/www/html -type f -exec sudo chmod 0664 {} \;
